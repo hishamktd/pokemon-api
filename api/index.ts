5b0b0bff1a4219812bd1002ea267ac844d770865
Hisham <hishammuhammed.in@gmail.com>
2025-02-03 20:04:47 +0530

    refactor API entry point to use NestJS with Express adapter and update Vercel configuration for routing

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,21 @@
-import app from '../src/main';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-export default app;
+const server: express.Express = express();
+
+export const createNestApp = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app;
+};
+
+createNestApp(server)
+  .then((app) => app.init())
+  .catch((err) => console.error('Nest broken', err));
+
+export default server;
