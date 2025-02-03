9558310edfaa2b5d62f4917d9ed93b65169edad6
Hisham <hishammuhammed.in@gmail.com>
2025-02-03 18:08:27 +0530

    remove .env file and add DATABASE_URL to configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,5 +3,6 @@ export default () => ({
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    connectionUrl: process.env.DATABASE_URL,
   },
 });
