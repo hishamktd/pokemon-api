45bb59e1abac9de847b096375132b804566470a1
Hisham <hishammuhammed.in@gmail.com>
2025-02-05 12:23:29 +0530

    Add ExpansionsController to ExpansionsModule providers

---
+++ src/masters/expansions/expansions.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { ExpansionsService } from './expansions.service';
+import { PageOptionsDto } from '@/common/dtos/page-opt-dtos';
+import { PageDto } from '@/common/page/page.dto';
+import { Expansion } from './expansions.entity';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Master Data - Expansions')
+@Controller('masters/expansions')
+export class ExpansionsController {
+  constructor(private readonly expansionsService: ExpansionsService) {}
+
+  @Get()
+  async findAll(
+    @Query() pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<Expansion>> {
+    return this.expansionsService.findAll(pageOptionsDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Expansion> {
+    return this.expansionsService.findOne(+id);
+  }
+
+  @Post()
+  async create(@Body() expansion: Expansion): Promise<Expansion> {
+    return this.expansionsService.create(expansion);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() expansion: Expansion,
+  ): Promise<Expansion> {
+    return this.expansionsService.update(+id, expansion);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.expansionsService.delete(+id);
+  }
+}
--- src/masters/expansions/expansions.module.ts
+++ src/masters/expansions/expansions.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExpansionsRepository } from './expansions.repository';
 import { ExpansionsService } from './expansions.service';
+import { ExpansionsController } from './expansions.controller';
 
 @Module({
-  providers: [ExpansionsRepository, ExpansionsService],
+  providers: [ExpansionsRepository, ExpansionsService, ExpansionsController],
   exports: [],
 })
 export class ExpansionsModule {}
