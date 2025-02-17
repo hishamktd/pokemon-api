87209817598bba21deb663cd8bf79e4663168ac6
Hisham <hishammuhammed.in@gmail.com>
2025-02-17 09:34:22 +0530

    Add FileUploadService to ExpansionsModule providers

--- src/masters/expansions/expansions.module.ts
+++ src/masters/expansions/expansions.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { FileUploadService } from '@/common/services/file-upload.service';
+
 import { ExpansionsController } from './expansions.controller';
 import { ExpansionsRepository } from './expansions.repository';
 import { ExpansionsService } from './expansions.service';
 
 @Module({
-  providers: [ExpansionsRepository, ExpansionsService],
+  providers: [ExpansionsRepository, ExpansionsService, FileUploadService],
   controllers: [ExpansionsController],
   exports: [],
 })
