8a4c1c0d0f326211a2b75a09e11e950d891ffa60
Hisham <hishammuhammed.in@gmail.com>
2025-03-01 18:27:43 +0530

    feat: add PokemonService to providers in PokemonModule

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonController } from './pokemon.controller';
 import { Pokemon } from './pokemon.entity';
 import { PokemonRepository } from './pokemon.repository';
+import { PokemonService } from './pokemon.service';
 import { TypesModule } from '../masters/types/types.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pokemon]), TypesModule],
-  providers: [PokemonRepository],
+  providers: [PokemonRepository, PokemonService],
   exports: [TypeOrmModule],
   controllers: [PokemonController],
 })
